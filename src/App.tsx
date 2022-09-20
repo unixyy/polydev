90c9bd29efd5baab225e1fde18a9c408beb5e66e
Richard MARTIN <rclic66000@gmail.com>
2022-09-20 15:07:02 +0200

    setting up the Dokku

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Polydev.App</p>
+        <p className="App-title">Polydev</p>
       </header>
     </div>
   );
